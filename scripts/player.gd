6be62aa94d167a3d71d4fb0bb1ebc43e8c5f7fb7
Nico Ville Valtteri Rajala <s4rani04@students.osao.fi>
2024-09-04 12:35:49 +0300

    character sprite

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export_range(0,1) var acceleration = 0.1
+@export_range(0,1) var deceleration = 0.1
 
+var run_speed = 250.0
+
+const WALK_SPEED = 150.0
+const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,12 +17,17 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("mv_left", "mv_right")
 	if direction:
-		velocity.x = direction * SPEED
+		if Input.is_action_pressed("run"):
+			velocity.x = move_toward(velocity.x, direction * run_speed, run_speed * acceleration)
+		else:
+			velocity.x = move_toward(velocity.x, direction * WALK_SPEED, WALK_SPEED * acceleration)
+		if velocity.x > 0:
+			$AnimatedSprite2D.animation = "run_l"
+		elif velocity.x < 0:
+			$AnimatedSprite2D.animation = "run_r"
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, WALK_SPEED * deceleration)
 
 	move_and_slide()
